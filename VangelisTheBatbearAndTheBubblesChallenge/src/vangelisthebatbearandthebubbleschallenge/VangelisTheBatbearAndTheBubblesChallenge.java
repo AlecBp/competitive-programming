9b9b1185fa432c8a385ec5402b003b6cb194049a
AlecBp <alec.paglia@gmail.com>
2020-02-03 13:18:00 -0400

    New optimized and finished program

--- VangelisTheBatbearAndTheBubblesChallenge/src/vangelisthebatbearandthebubbleschallenge/VangelisTheBatbearAndTheBubblesChallenge.java
+++ VangelisTheBatbearAndTheBubblesChallenge/src/vangelisthebatbearandthebubbleschallenge/VangelisTheBatbearAndTheBubblesChallenge.java
@@ -7,27 +7,36 @@ import java.util.Scanner;
 public class VangelisTheBatbearAndTheBubblesChallenge {
 
     public static void main(String[] args) {
-
         Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
+        int nodes, edges, n1, n2, n;
+        boolean hasLoop;
+        int[][] table;
+        n = in.nextInt();
+
         for (int t = 0; t < n; t++) {
-            int nodes = in.nextInt();
-            int edges = in.nextInt();
-            int n1, n2;
-            int[][] table = new int[nodes][nodes];
+            hasLoop = false;
+            nodes = in.nextInt();
+            edges = in.nextInt();
+            table = new int[nodes][nodes];
+
             for (int i = 0; i < edges; i++) {
                 n1 = in.nextInt();
                 n2 = in.nextInt();
-                // Eliminate self loops
+                // Check for self loops
                 if (n1 == n2) {
-                    System.out.println("1");
-                    continue;
+                    hasLoop = true;
                 }
                 table[n1][n2] = 1;
                 table[n2][n1] = 1;
             }
-            if (findLoop(table)) {
-                System.out.println("1");
+
+            // If there was a self loop in the input, print 0 and go to next
+            if (!hasLoop) {
+                if (findLoop(table)) {
+                    System.out.println("1");
+                } else {
+                    System.out.println("0");
+                }
             } else {
                 System.out.println("0");
             }
@@ -35,8 +44,8 @@ public class VangelisTheBatbearAndTheBubblesChallenge {
     }
 
     public static boolean findLoop(int[][] table) {
+        boolean[] visited = new boolean[table.length];
         for (int i = 0; i < table.length; i++) {
-            boolean[] visited = new boolean[table.length];
             if (findLoop(table, visited, i, i)) {
                 return true;
             }
@@ -49,21 +58,26 @@ public class VangelisTheBatbearAndTheBubblesChallenge {
         if (visited[currNode]) {
             return true;
         }
+        // Mark current node as visited
+        visited[currNode] = true;
         // Check for options to go to
         for (int i = 0; i < table.length; i++) {
+            // Must be different than self
             if (i != currNode) {
+                // Must be connected to that node
                 if (table[currNode][i] == 1) {
+                    // Must be different than the previous node
                     if (i != prevNode) {
-                        visited[currNode] = true;
-//                        System.out.println("FROM " + currNode + " TO " + i);
+                        // System.out.println("FROM " + currNode + " TO " + i);
                         if (findLoop(table, visited, currNode, i)) {
                             return true;
                         }
-                        visited[currNode] = false;
                     }
                 }
             }
         }
+        // Mark current node as not visited
+        visited[currNode] = false;
         return false;
     }
 
